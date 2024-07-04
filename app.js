238c8a1c6872a31861b09dd0ea5b8a660b4c7508
wbmnstr <fatihsaridag@gmail.com>
2024-07-05 00:21:34 +0300

    "Updated altbaslik text in content.json"

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const PORT=5000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const content = require('./content.json');
 const app = express();
 
+app.use(bodyParser.json({limit:"50mb"}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit:50000 }));
 app.set('view engine','pug');
 app.set('views','./sayfalar');
 app.use(express.static('public'));
 
 app.get('/',(req, res)=>{
-    res.render('index');
+    console.log(content);
+    res.render('index', { content });
 });
+
+app.get('/administrationpage',(req,res)=>{
+    res.render('administrationpage', { content });
+})
+app.post('/administrationpage',(req,res)=>{    
+        fs.writeFile('content.json', JSON.stringify(req.body), (err)=>{
+            if(err) throw err;
+            console.log('Dosya yazıldı');   
+        })        
+    
+   console.log(req.body);
+    res.render('postAdministrationpage');
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`System is running on http://localhost:${PORT}`);
